439643190ea52a80423f8af08a69a5a09c897033
Chinmoy Pathak <pathak.work.2025@gmail.com>
2026-09-14 18:22:02 +0530

    add the local existing repo to the github

---
+++ dist/core/strawberryCore.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 10;
+console.log(age);
+//# sourceMappingURL=strawberryCore.js.map
---
+++ dist/proxy/strawberry.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "kashyap";
+console.log(name);
+//# sourceMappingURL=strawberry.js.map
